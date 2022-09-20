a7deb1abd4254911ea2ecdd156632134ef42d606
Pierce Brooks <piercebrooks@rocketmail.com>
2022-09-20 14:53:46 -0400

    remove build

--- build/src/CMakeFiles/object.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "object.pdb"
-  "object"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/object.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
